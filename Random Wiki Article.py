c927021bf113a64371ad8827a29d922385493569
Stanislav Finko <ghostzip8@gmail.com>
2021-12-01 20:37:46 +0200

    Update .py file. Fit spaces according to PEP8.

--- Random Wiki Article.py
+++ Random Wiki Article.py
@@ -1,6 +1,7 @@
 import requests
 import bs4
 
+
 def creating_site_object():
     try:
         return requests.get('https://en.wikipedia.org/wiki/Special:Random')
@@ -8,11 +9,14 @@ def creating_site_object():
         print('Ops! There is a problem with internet connection.')
         exit()
 
+
 def taking_article_title(site_object):
     return site_object.url[30:].replace('_', ' ')
 
+
 def suggest_article_to_user(article_title):
     return input('Do you wanna read about "' + article_title + '"? (press "Y" for yes, "N" for no): ')
+
 
 def take_content(site_object):
     html = bs4.BeautifulSoup(site_object.text, 'html.parser')
@@ -20,6 +24,7 @@ def take_content(site_object):
     html = html[0].find_all(['ul', 'p', 'h1', 'h2', 'h3', 'table'])
     for text in html:
         yield text.get_text()
+
 
 def output_content(content):
     print()
